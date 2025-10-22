430fb23977107cd3f6baf9290bf1c7590142401b
manohar-aim <manoharaj2018@gmail.com>
2025-10-22 07:43:59 +0530

    variables

---
+++ 04-conversation.sh
@@ -0,0 +1,8 @@
+#!/bin/bash 
+
+PERSON1=$1
+PERSON2=$2
+echo "$PERSON1:: welcome to america"
+echo "$PERSON2:: thank you $PERSON1"
+echo "$PERSON1:: how are you $PERSON2"
+echo "$PERSON2:: I am good thank you "
