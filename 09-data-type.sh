3449a36f13156c254b289335e328673eecf1c1fc
manohar-aim <manoharaj2018@gmail.com>
2025-10-25 16:19:48 +0530

    variable1

--- 09-data-type.sh
+++ 09-data-type.sh
@@ -13,11 +13,12 @@ VALIDATE(){
     
     if [ $1 -ne 0 ]; then
 
-    echo "error::Installing $2 is failure"
-    exit 1  
+        echo "error::Installing $2 is failure"
+        exit 1  
 
     else
-    echo "installing $2 is success"  
+        echo "installing $2 is success"  
+    fi
 }
 
 dnf install mysql -y
