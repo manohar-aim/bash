61907844acf8078bf8094525934f067fe8bf2af3
manohar-aim <manoharaj2018@gmail.com>
2025-10-25 14:25:31 +0530

    variable1

---
+++ 09-data-type.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+USERID = $(id -u)
+
+if [ $USERID -ne 0 ]; then
+    
+    echo "Error:: please run this script with root previlage"
+fi
+
+dnf install mysql -y
+
+if [ $? -ne 0 ]; then
+
+    echo "error::Installing mysql is failure"
+
+else
+    echo "installing mysql is success"
+
+fi
