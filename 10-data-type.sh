362240ac2bea137385575c961510d9af48ed719c
manohar-aim <manoharaj2018@gmail.com>
2025-10-25 16:28:02 +0530

    variable1

---
+++ 10-data-type.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then 
+    echo "ERROR::please run the script with root previlage"
+fi
+
+VALIDATE(){
+    if ($1 -ne 0);then
+        echo "Installing $2 is failed"
+    else
+        echo "Installing $2 is Success"
+    fi
+}
+
+dnf install python3 -y
+VALIDATE $? "Python3"
+
